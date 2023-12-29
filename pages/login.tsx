b7c95eb4e98ffd53b2138378bc177faf67a56d23
Sanket <sknangare@mitaoe.ac.in>
2023-12-29 20:12:06 +0530

    fix: Ifelse-login

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
-  const [isSignUpMode, setIsSignUpMode] = useState(false); // State to toggle between sign-up and login
+  const [isSignUpMode, setIsSignUpMode] = useState(false); 
   const formData = new FormData();
-  formData.append('username', username);
-  formData.append('password', password);
+  formData.append("username", username);
+  formData.append("password", password);
   const router = useRouter();
 
   const isUsernameValid = /^[a-zA-Z0-9_-]{3,16}$/.test(username);
@@ -18,30 +18,30 @@ const Login: React.FC = () => {
 
   const handleAction = async () => {
     if (!isUsernameValid || !isPasswordValid) {
-      setError('Invalid username or password format.');
+      setError("Invalid username or password format.");
       return;
     }
 
     // Handle login logic
     try {
-      const response = await fetch('/api/passport', {
-        method: 'POST',
+      const response = await fetch("/api/passport", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password, isSignUpMode }),
       });
 
       if (response.ok) {
-        console.log('Login successful!');
-        router.push('/try-interview');
+        console.log("Login successful!");
+        router.push("/try-interview");
       } else {
         const errorData = await response.json();
         setError(errorData.message);
       }
     } catch (error: any) {
-      console.error('Login failed:', error);
-      setError('An unexpected error occurred.');
+      console.error("Login failed:", error);
+      setError("An unexpected error occurred.");
     }
   };
 
@@ -53,9 +53,16 @@ const Login: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 shadow-md rounded-md w-96">
-        <Image src="/WorqHat TM Logo.png" width={150} height={50} alt="WorqHat Logo" />
+        <Image
+          src="/WorqHat TM Logo.png"
+          width={150}
+          height={50}
+          alt="WorqHat Logo"
+        />
 
-        <h2 className="text-2xl font-semibold mb-6">{isSignUpMode ? 'Sign Up' : 'Login'}</h2>
+        <h2 className="text-2xl font-semibold mb-6">
+          {isSignUpMode ? "Sign Up" : "Login"}
+        </h2>
 
         {error && <p className="text-red-500 mb-4">{error}</p>}
 
@@ -83,18 +90,29 @@ const Login: React.FC = () => {
           className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
           onClick={handleAction}
         >
-          {isSignUpMode ? 'Sign Up' : 'Login'}
+          {isSignUpMode ? "Sign Up" : "Login"}
         </button>
 
-        <p className="mt-4">
-          {isSignUpMode ? 'Already have an account?' : "Don't have an account?"}{' '}
-          <button className="text-blue-500" onClick={toggleMode}>
-            {isSignUpMode ? 'Login' : 'Sign Up'}
-          </button>
-        </p>
+        {isSignUpMode ? (
+          <p className="mt-4">
+            Already have an account ? {" "}
+            <button className="text-blue-500" onClick={toggleMode}>
+              Login
+            </button>
+          </p>
+        ) : (
+          <p className="mt-4">
+            Create an account{" "}
+            <button className="text-blue-500" onClick={toggleMode}>
+              Sign Up
+            </button>
+          </p>
+        )}
       </div>
     </div>
   );
 };
+
+
 
 export default Login;
